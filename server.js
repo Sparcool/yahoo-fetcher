c0493689d2dd57ef76310e38d66bac10531a8aff
Justin Bériot <justin.beriot@gmail.com>
2016-07-16 12:14:14 +0200

    feat: parse the first page

--- server.js
+++ server.js
@@ -11,7 +11,19 @@ let url = 'https://in.finance.yahoo.com/q/hp?s=AAPL&a=00&b=01&c=1991&d=04&e=4&f=
 let fetcher = new Fetcher(url);
 
 fetcher.fetch()
-    .then(ht => {
+    .then(html => {
+        let $ = cheerio.load(html);
+        let data = '';
+        $('table.yfnc_datamodoutline1 tr:not(:first-child):not(:last-child)').each(function() {
+            let entry = $(this);
+            if (entry.children().length === 7) {
+                let date = new Date(entry.children().first().text()).toISOString();
+                let close = entry.children().eq(4).text();
+                let volume = (entry.children().eq(5).text()).replace(/\,/g,'');
+
+                data += `${date},${close},${volume}\n`;
+            }
+        })
         return fs.writeFileAsync('fetch.txt', data);
     })
     .then(() => {
